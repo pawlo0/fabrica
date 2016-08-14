b08b12e2ce61156e59ed03c4b9dc8517ae385052
Paulo Santos <sungonku@gmail.com>
2016-08-14 10:08:37 +0000

    improves navbar: adds logo and redirects to root route when loggingOut

--- imports/ui/navbar.js
+++ imports/ui/navbar.js
@@ -1,6 +1,7 @@
 import { Modal } from 'meteor/peppelg:bootstrap-3-modal';
 import { AccountsTemplates } from 'meteor/useraccounts:core';
 import { T9n } from 'meteor/softwarerero:accounts-t9n';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import '../api/users.js';
 import './navbar.html';
@@ -20,6 +21,7 @@ Template.navbar.events({
     },
     'click .js-logoutButton'(event){
         event.preventDefault();
+        FlowRouter.go('/');
         AccountsTemplates.logout();
     }
 });
